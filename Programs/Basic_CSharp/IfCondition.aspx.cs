51fec745e62b83cadf9d84c3f0bc719fb05e1eb4
abhishekworkspace <36187249+abhishekworkspace@users.noreply.github.com>
2021-03-17 18:55:41 +0530

    Update for loop conditions
    looping and conditions update

--- Programs/Basic_CSharp/IfCondition.aspx.cs
+++ Programs/Basic_CSharp/IfCondition.aspx.cs
@@ -9,6 +9,10 @@ public partial class IfCondition : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-      
+        int i = 10, j = 20;
+        if (i > j)
+            HttpContext.Current.Response.Write("Difference between i an d j : " + (i - j));
+        else
+            HttpContext.Current.Response.Write("Difference between i an d j : " + (j - i));
     }
 }
---
+++ Programs/Basic_CSharp/Looping.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Looping : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for(int j=10; j<i; j++)
+            {
+                if (i % j == 0)
+                    HttpContext.Current.Response.Write("<br> : " + (j + i));
+            }
+        }
+    }
+}
